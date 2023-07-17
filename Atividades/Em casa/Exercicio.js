a9412f5069f0d2a9d103aebc3a3847444824895d
DoriansFer <doohfernandes@yahoo.com>
2023-07-17 13:06:50 -0300

    Faltando apenas exercicio 5 função chamar()

--- Atividades/Em casa/Exercicio.js
+++ Atividades/Em casa/Exercicio.js
@@ -2,7 +2,7 @@
 
 
 class Animal {
-
+    static chama = []
     constructor(nome, idade, cor) {
         this.nome = nome
         this.idade = idade
@@ -10,69 +10,97 @@ class Animal {
         this.consultas = []
         this.vacinas = []
         this.social = false
-
-
     }
 
     consultar() {
-        this.consultas.push(new Date())
+        this.consultas.unshift(new Date())
     }
 
     vacinar(value) {
         this.vacinas.push(value)
-        this.consultas.push(new Date())
+        this.consultas.unshift(new Date())
+    }
+
+    chamar() {
+        if (this.social == true || this.especie == "cachorro") {
+            return Animal.chama
+        }
+    }
+
+    alimentar() {
+        this.social = true
+        Animal.chama.unshift(this.nome)
+        console.log(`${this.nome} agora confia em você!`)
     }
 
 
 }
 
 class Cat extends Animal {
+
     constructor(nome, idade, cor, castrado, externo) {
         super(nome, idade, cor)
         this.castrado = castrado
         this.externo = externo
+        this.especie = "gato"
     }
     miar() {
         console.log("miau miau")
+    }
+
+    brincar() {
+        console.log("gatinhos gostam de brincar de novelo de lã")
     }
 
     acariciar() {
         if (this.social) {
             console.log("ron ron ron")
         } else {
-            console.log(`Atenção, ${this.nome} rosnou para você!`)
+            console.log(`Atenção, ${this.nome}, rosnou para você!`)
         }
     }
-    alimentar() {
-        this.social = true
-        console.log(`${this.nome} agora confia em você!`)
+
+    alimentar(value) {
+        if (value == "peixe" || value == "frango" || value == "sachê") {
+            console.log(`${this.nome}, agora confia em você!`)
+            Animal.chama.unshift(this.nome)
+            return this.social = true
+        } else {
+            console.log(`${this.nome}, não gosta de ${value}`)
+        }
     }
 
+
     castrar() {
-        if (castrado = true) {
-            console.log(`Atenção! ${this.nome}, já está castrado.`)
+        if (this.castrado == true) {
+            console.log(`Atenção! ${this.nome}, já está castrado(a).`)
         } else {
-            console.log(`${this.nome}, foi castrado com sucesso!!`)
-            return castrado = true
+            console.log(`${this.nome}, foi castrado(a) com sucesso!!`)
+            this.consultas.unshift(new Date())
+            return this.castrado = true
         }
     }
 
 }
 
+// let Lua = new Cat("Lua", 2, "preta", true, false)
+
 class Dog extends Animal {
+
     #ferido
     constructor(nome, idade, cor, castrado, raça, ferido) {
         super(nome, idade, cor)
         this.castrado = castrado
         this.#ferido = ferido
         this.raça = raça
+        this.especie = "cachorro"
     }
     latir() {
         console.log("au au")
     }
 
     brincar() {
-        if (ferido == true) {
+        if (this.#ferido == true) {
             console.log("Esse doguinho está ferido e não sairá para brincar")
         } else {
             console.log(`${this.nome} gosta de brincar com bolinha`)
@@ -86,17 +114,15 @@ class Dog extends Animal {
             console.log(`Atenção, ${this.nome} rosnou para você!`)
         }
     }
-    alimentar() {
-        this.social = true
-        console.log(`${this.nome} agora confia em você!`)
-    }
+
 
     castrar() {
-        if (castrado = true) {
-            console.log(`Atenção! ${this.nome}, já está castrado.`)
+        if (this.castrado == true) {
+            console.log(`Atenção! ${this.nome}, já está castrado(a).`)
         } else {
-            console.log(`${this.nome}, foi castrado com sucesso!!`)
-            return castrado = true
+            console.log(`${this.nome}, foi castrado(a) com sucesso!!`)
+            this.consultas.unshift(new Date())
+            return this.castrado = true
         }
     }
 }
@@ -104,10 +130,12 @@ class Dog extends Animal {
 // let Rock = new Dog("Rock", 2, "preto", false, "vira-lata", true)
 
 class Hamster extends Animal {
+
     constructor(nome, idade, cor, tipo) {
         super(nome, idade, cor)
         this.tipo = tipo
         this.estressado = true
+        this.especie = "hamster"
     }
 
     brincar() {
@@ -130,15 +158,17 @@ class Hamster extends Animal {
             console.log(`Atenção, ${this.nome}, mordeu você!`)
         }
     }
-    alimentar() {
-        this.social = true
-        console.log(`${this.nome} agora confia em você!`)
-    }
+
 }
 
 // let ronaldinho = new Hamster("Ronaldinho",3,"malhado","sírio")
 
 class Papagaio extends Animal {
+
+    constructor(nome, idade, cor) {
+        super(nome, idade, cor)
+        this.especie = "papagaio"
+    }
 
     falar() {
         console.log("Loro, quer biscoito?")
@@ -152,4 +182,14 @@ class Papagaio extends Animal {
         console.log(`${this.nome}, não quer brincar com você`)
     }
 
+    acariciar() {
+        if (this.social) {
+            console.log(`${this.nome}, deixou você acariciar`)
+        } else {
+            console.log(`Atenção, ${this.nome}, bicou você!`)
+        }
+    }
+
 }
+
+//let Loro = new Papagaio("Louro", 10, "verde")
--- Atividades/Em casa/Exercicios.md
+++ Atividades/Em casa/Exercicios.md
@@ -22,9 +22,56 @@ Nossa veterinária agora atende quatro espécies diferentes de animais: dois dom
     acariciar(),
     alimentar(),
 }
+
 ```
  - Gatos sociáveis ronronam ao serem acariciados. Se um gato não for sociável, ele deve silvar.
  - Você pode tornar um gato sociável alimentando ele com uma das comidas favoritas dos gatos: frango, sachê ou peixe.
+
+ Resposta: 
+ 
+ class Cat extends Animal {
+    constructor(nome, idade, cor, castrado, externo) {
+        super(nome, idade, cor)
+        this.castrado = castrado
+        this.externo = externo
+    }
+    miar() {
+        console.log("miau miau")
+    }
+
+    brincar() {
+        console.log("gatinhos gostam de brincar de novelo de lã")
+    }
+
+    acariciar() {
+        if (this.social) {
+            console.log("ron ron ron")
+        } else {
+            console.log(`Atenção, ${this.nome}, rosnou para você!`)
+        }
+    }
+
+    alimentar(value) {
+        if (value == "peixe" || value == "frango" || value == "sachê") {
+            console.log(`${this.nome}, agora confia em você!`)
+            return this.social = true
+        } else {
+            console.log(`${this.nome}, não gosta de ${value}`)
+        }
+    }
+
+
+    castrar() {
+        if (this.castrado == true) {
+            console.log(`Atenção! ${this.nome}, já está castrado(a).`)
+        } else {
+            console.log(`${this.nome}, foi castrado(a) com sucesso!!`)
+            this.consultas.unshift(new Date())
+            return this.castrado = true
+        }
+    }
+
+}
 
  2. Cachorro
  ```
@@ -49,6 +96,45 @@ Nossa veterinária agora atende quatro espécies diferentes de animais: dois dom
  ```
   - Cachorros costumam esconder quando estão feridos. Use um campo privado para essa informação, e só a exiba ao tentar `brincar()` com um cãozinho que está ferido.
 
+  RESPOSTA: class Dog extends Animal {
+    #ferido
+    constructor(nome, idade, cor, castrado, raça, ferido) {
+        super(nome, idade, cor)
+        this.castrado = castrado
+        this.#ferido = ferido
+        this.raça = raça
+    }
+    latir() {
+        console.log("au au")
+    }
+
+    brincar() {
+        if (this.#ferido == true) {
+            console.log("Esse doguinho está ferido e não sairá para brincar")
+        } else {
+            console.log(`${this.nome} gosta de brincar com bolinha`)
+        }
+    }
+
+    acariciar() {
+        if (this.social) {
+            console.log(`${this.nome}, abanou o rabo`)
+        } else {
+            console.log(`Atenção, ${this.nome} rosnou para você!`)
+        }
+    }
+
+
+    castrar() {
+        if (this.castrado == true) {
+            console.log(`Atenção! ${this.nome}, já está castrado(a).`)
+        } else {
+            console.log(`${this.nome}, foi castrado(a) com sucesso!!`)
+            this.consultas.unshift(new Date())
+            return this.castrado = true
+        }
+    }
+
 3. Hamster
 ```
 {
@@ -66,6 +152,37 @@ Nossa veterinária agora atende quatro espécies diferentes de animais: dois dom
 ```
   - Ao consultar um hamster, você deve brincar com ele para que ele não fique estressado e tente fugir.
 
+  class Hamster extends Animal {
+    constructor(nome, idade, cor, tipo) {
+        super(nome, idade, cor)
+        this.tipo = tipo
+        this.estressado = true
+    }
+
+    brincar() {
+        this.estressado = false
+        console.log(`${this.nome}, gosta de brincar na rodinha`)
+    }
+
+    consultar() {
+        if (this.estressado == false) {
+            this.consultas.push(new Date())
+        } else {
+            console.log(`Eita, ${this.nome} fugiu!!`)
+        }
+    }
+
+    acariciar() {
+        if (this.social) {
+            console.log(`${this.nome}, balançou o bumbunzinho`)
+        } else {
+            console.log(`Atenção, ${this.nome}, mordeu você!`)
+        }
+    }
+   
+}
+
+
 4. Papagaio
 ```
 {
@@ -82,6 +199,30 @@ Nossa veterinária agora atende quatro espécies diferentes de animais: dois dom
 }
 ```
  - Ao brincar com ou alimentar um papagaio, ele deve falar com você.
+
+ class Papagaio extends Animal {
+
+    falar() {
+        console.log("Loro, quer biscoito?")
+    }
+
+    alimentar() {
+        console.log(`${this.nome}, quer mais`)
+    }
+
+    brincar() {
+        console.log(`${this.nome}, não quer brincar com você`)
+    }
+
+    acariciar() {
+        if (this.social) {
+            console.log(`${this.nome}, deixou você acariciar`)
+        } else {
+            console.log(`Atenção, ${this.nome}, bicou você!`)
+        }
+    }
+
+}
 
 5. Outros
   - Sempre que vacinar um paciente, você deve adicionar uma nova consulta na lista. O mesmo vale para castrar um animal.
