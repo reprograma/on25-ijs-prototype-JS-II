f6b3a4c61fbc0ca44d4234b3a8e0c10ab5643b01
carolmarquezini <c.marquezini15@gmail.com>
2023-07-19 22:44:05 -0300

    criando animal doméstico, fazendo eles brincar, alimentar e castrar

--- Atividades/Em casa/exercicio.js
+++ Atividades/Em casa/exercicio.js
@@ -1,34 +1,93 @@
-function HistoricoMedico() {
-    this.consultas = [];
-    this.vacinas = [];
-}
-
 class Animal {
-    constructor(nome, idade, cor, castrado, raca) {
-        this.nome = nome
-        this.idade = idade
-        this.cor = cor
-        this.consultas = [] 
-        this.castrado = true
-        this.raca = raca
-    }
+  constructor(nome, idade, cor) {
+    this.nome = nome;
+    this.idade = idade;
+    this.cor = cor;
+    this.consultas = [];
+  }
 
-    consulta() {
-        this.consultas.push(new Date())
-    }
+  consultar() {
+    this.consultas.push(new Date());
+  }
+
+  brincar(brincadeira) {
+    console.log(`brincando de ${brincadeira} com ${this.nome}`);
+  }
+
+  acariciar() {
+    console.log(`Você está acariciando o(a) ${this.nome} *-*`);
+  }
+
+  alimetar() {
+    console.log(`Você está alimentando o(a) ${this.nome} :)`);
+  }
 }
 
-class Gatos extends Animal  {
-    miar() {
-        console.log("miau miau")
-    }
+class Domestico extends Animal {
+  constructor(nome, idade, cor) {
+    super(nome, idade, cor);
+
+    this.castrado = false;
+    this.vacinas = [];
+    this.consultas = [];
+  }
+
+  vacinar(vacina) {
+    this.vacinas.push(vacina);
+    this.consultar();
+  }
+
+  castrar(data) {
+    this.castrado = true;
+    this.consultar(data);
+  }
 }
 
-class Cachorros extends Animal {
-    latir(){
-        console.log("auu auu")
+class Gatos extends Domestico {
+  constructor(nome, idade, cor) {
+    super(nome, idade, cor);
+  }
+
+  miar() {
+    console.log('miau miau');
+  }
+
+  alimetar(comida) {
+    if ((comida == 'frango') | (comida == 'sachê') | (comida == 'peixe')) {
+      console.log('gato alimetado e sociável!!!');
+    } else {
+      console.log('gato alimentado mas não sociável!');
     }
+  }
 }
 
-let boris = new Cachorros("Bóris", 36, "branco", "shitzu") //idade em meses
+let charmoso = new Gatos('Charmoso', 12, 'branco'); //idade em meses
 
+class Cachorros extends Domestico {
+  #ferido;
+
+  constructor(nome, idade, cor, raca) {
+    super(nome, idade, cor);
+
+    this.raca = raca;
+    this.#ferido = false;
+  }
+
+  latir() {
+    console.log('auu auu');
+  }
+
+  mudarFerido(ferido) {
+    this.#ferido = ferido;
+  }
+
+  brincar(brincadeira) {
+    if (this.#ferido) {
+      console.log('cachorro feriado, não pode brincar.');
+      return;
+    }
+    console.log(`brincando de ${brincadeira} com ${this.nome}`);
+  }
+}
+
+let boris = new Cachorros('Bóris', 36, 'branco', 'shitzu'); //idade em meses
