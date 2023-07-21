d615deb880cba6d9ec825daf4122e659ad3f8bf7
unknown <elvirabrunoleme@gmail.com>
2023-07-21 08:54:01 -0300

    Atividade semana 4 completa

--- Atividades/Em casa/atividade.js
+++ Atividades/Em casa/atividade.js
@@ -1,91 +1,91 @@
 class HistoricoMedico {
-    constructor() {
-      this.consultas = [];
-      this.vacinas = [];
-      this.procedimento = [];
+  constructor() {
+    this.consultas = [];
+    this.vacinas = [];
+    this.procedimento = [];
+  }
+}
+
+class Animal {
+  constructor(nome, idade, cor, consultas = []) {
+    this.nome = nome;
+    this.idade = idade;
+    this.cor = cor;
+    this.historico = new HistoricoMedico();
+  }
+
+  consultar() {
+    const dataConsulta = new Date();
+    this.consultas.push(dataConsulta);
+    this.historico.consultas.push(dataConsulta);
+  }
+
+  registrarProcedimento(procedimento) {
+    this.historico.procedimento.push(procedimento);
+  }
+
+  brincar() {
+    console.log(`${this.nome} está brincando.`);
+  }
+
+  acariciar() {
+    console.log(`${this.nome} está sendo acariciado.`);
+  }
+
+  alimentar() {
+    console.log(`${this.nome} está sendo alimentado.`);
+  }
+}
+
+class Gato extends Animal {
+  constructor(nome, idade, cor, consultas = []) {
+    super(nome, idade, cor, consultas);
+    this.social = true;
+  }
+
+  miar() {
+    if (this.social) {
+      console.log(`${this.nome} está ronronando.`);
+    } else {
+      console.log(`${this.nome} está sibilando.`);
     }
   }
-  
-  class Animal {
-    constructor(nome, idade, cor, consultas = []) {
-      this.nome = nome;
-      this.idade = idade;
-      this.cor = cor;
-      this.historico = new HistoricoMedico();
-    }
-  
-    consultar() {
-      const dataConsulta = new Date();
-      this.consultas.push(dataConsulta);
-      this.historico.consultas.push(dataConsulta);
-    }
-  
-    registrarProcedimento(procedimento) {
-      this.historico.procedimento.push(procedimento);
-    }
-  
-    brincar() {
-      console.log(`${this.nome} está brincando.`);
-    }
-  
-    acariciar() {
-      console.log(`${this.nome} está sendo acariciado.`);
-    }
-  
-    alimentar() {
-      console.log(`${this.nome} está sendo alimentado.`);
+
+  vacinar(vacina) {
+    this.historico.vacinas.push(vacina);
+  }
+
+
+  brincar() {
+    console.log(`${this.nome} está brincando com uma bola de lã.`);
+  }
+
+  acariciar() {
+    super.acariciar();
+    if (this.social) {
+      console.log(`${this.nome} está ronronando de felicidade.`);
     }
   }
-  
-  class Gato extends Animal {
-    constructor(nome, idade, cor, consultas = []) {
-      super(nome, idade, cor, consultas);
-      this.social = true;
-    }
-  
-    miar() {
-      if (this.social) {
-        console.log(`${this.nome} está ronronando.`);
-      } else {
-        console.log(`${this.nome} está sibilando.`);
+
+  alimentar(comida) {
+    super.alimentar();
+    const comidasFavoritas = ["frango", "sachê", "peixe"];
+    let encontrouComidaFavorita = false;
+
+    for (let i = 0; i < comidasFavoritas.length; i++) {
+      if (comidasFavoritas[i] === comida) {
+        encontrouComidaFavorita = true;
+        break;
       }
-    }
-  
-    vacinar(vacina) {
-      this.historico.vacinas.push(vacina);
     }
 
-  
-    brincar() {
-      console.log(`${this.nome} está brincando com uma bola de lã.`);
+    if (this.social && encontrouComidaFavorita) {
+      console.log(`${this.nome} está mais sociável agora.`);
+      this.social = true;
     }
-  
-    acariciar() {
-      super.acariciar();
-      if (this.social) {
-        console.log(`${this.nome} está ronronando de felicidade.`);
-      }
-    }
-  
-    alimentar(comida) {
-        super.alimentar();
-        const comidasFavoritas = ["frango", "sachê", "peixe"];
-        let encontrouComidaFavorita = false;
-      
-        for (let i = 0; i < comidasFavoritas.length; i++) {
-          if (comidasFavoritas[i] === comida) {
-            encontrouComidaFavorita = true;
-            break;
-          }
-        }
-      
-        if (this.social && encontrouComidaFavorita) {
-          console.log(`${this.nome} está mais sociável agora.`);
-          this.social = true;
-        }
-      }
   }
-  
+}
+
 
 gato.miar(); // Saída: Nina está ronronando.
 gato.vacinar("gripe"); // Adiciona uma nova vacina e consulta
@@ -164,3 +164,70 @@ cachorro.acariciar(); // Saída: Zeus está sendo acariciado. Zeus está abanand
 cachorro.alimentar("frango"); // Saída: Zeus está sendo alimentado. Zeus está mais animado agora.
 cachorro.castrar(); // Saída: Zeus foi castrado.
 cachorro.castrar(); // Saída: Uncaught Error: Zeus já foi castrado.
+
+
+class Hamster extends Animal {
+  constructor(nome, idade, cor, consultas = []) {
+    super(nome, idade, cor, consultas);
+  }
+
+  consultar() {
+    super.consultar();
+    console.log(`${this.nome} está brincando para não ficar estressado.`);
+  }
+
+  brincar() {
+    console.log(`${this.nome} está correndo na rodinha.`);
+  }
+
+  acariciar() {
+    console.log(`${this.nome} está adorando os carinhos.`);
+  }
+
+  alimentar() {
+    console.log(`${this.nome} está comendo sementinhas.`);
+  }
+}
+
+const hamster = new Hamster("Iogurte", 1.5, "branco e laranja", []);
+
+hamster.brincar();
+hamster.alimentar();
+hamster.consultar();
+hamster.acariciar();
+
+
+class Papagaio extends Animal {
+
+  constructor(nome, idade, cor, consultas = []) {
+    super(nome, idade, cor, consultas);
+  }
+
+  falar() {
+    console.log(`${this.nome} está falando.`);
+  }
+
+  consultar() {
+    super.consultar();
+    console.log(`${this.nome} está falando enquanto você o consulta.`);
+  }
+
+  brincar() {
+    console.log(`${this.nome} está brincando e falando.`);
+  }
+
+  acariciar() {
+    console.log(`${this.nome} está adorando os carinhos e falando.`);
+  }
+
+  alimentar() {
+    console.log(`${this.nome} está comendo sementes e falando.`);
+  }
+}
+
+const papagaio = new Papagaio("José", 13, "verde", []);
+
+papagaio.brincar();
+papagaio.alimentar();
+papagaio.consultar();
+papagaio.acariciar();
