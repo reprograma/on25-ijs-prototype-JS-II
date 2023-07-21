ccd8b87b4b321187ce774f5d6a1c9c7ca6c4d9a5
Deborah Gomes Souza <deborah270691@gmail.com>
2023-07-21 19:21:08 -0300

    refatorando as funções da classe pai para arrow function porque sim.

--- Atividades/Em casa/deborah-gomes/veterinaria.js
+++ Atividades/Em casa/deborah-gomes/veterinaria.js
@@ -6,23 +6,34 @@ class Animal {
         this.consultas = consultas;
     }
 
-    consultar () {
+    consultar = () => {
         console.log(`${this.nome} foi consultado`);
     }
 
-    brincar() {
-        console.log()
+    brincar = () => {
+        console.log(`${this.nome} está brincando.`)
     }
 
-    acariciar() {
-        console.log(`${this.nome} está sendo acariciado.`)
+    acariciar = () => {
+        console.log(`${this.nome} está sendo acariciado.`);
     }
 
-    alimentar () {
-        console.log(`${this.nome} está comendo.`)
+    alimentar = () => {
+        console.log(`${this.nome} está comendo.`);
+    }
+}
+
+
+class Gato extends Animal {
+    constructor(nome, idade, cor, vacinas, consultas, castrado, externo, social) {
+        super(nome, idade, cor, consultas);
+        this.vacinas = vacinas;
+        this.castrado = castrado;
+        this.externo = externo;
+        this.social = social;
     }
 
-
-
-
+    miar() {
+        console.log(`${this.nome} está ${this.social ? "ronronando" : "silvando"}.`);
+    }
 }
